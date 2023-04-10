9ae5b1801359decd96c7b81cf332c87309d73d6f
rezzasakithati <rezza.sury@students.amikom.ac.id>
2023-04-10 23:39:24 +0700

    commit ke1

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace void_overload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            number numbers = new number();
+            numbers.CariNilaiMin(45, 21);
+            numbers.CariNilaiMin(27, 56, 54);
+            numbers.CariNilaiMax(23, 34);
+            numbers.CariNilaiMax(53, 51, 50);
+        }
+    }
+}
